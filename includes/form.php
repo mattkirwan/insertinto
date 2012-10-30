8417c0140bf7c0d087923b31e135849ade3096d1
Matt Kirwan <matthewkirwan@gmail.com>
2012-10-30 21:06:11 +0000

    Adding submit css

--- includes/form.php
+++ includes/form.php
@@ -13,28 +13,28 @@
 							<hr class="clear" />
 							<label for="key_2_name">Field Name (Key 2)</label>
 							<input type="text" class="text" name="key_2_name" value="" />		
-
+							<hr class="clear" />
 						</fieldset>
 
 						<fieldset>
 							<label for="from">Auto-generate From</label>
 							<input type="text" class="text" name="from" value="" />	
-
+							<hr class="clear" />
 							<label for="to">Auto-generate To</label>
 							<input type="text" class="text" name="to" value="" />
-
+							<hr class="clear" />
 						</fieldset>
 
 						<fieldset>
 							<label for="key_2_values">Relationship Value 1</label>
 							<input type="text" class="text" name="key_2_values[]" value="" />	
-
+							<hr class="clear" />
 							<label for="key_2_values">Relationship Value 2</label>
 							<input type="text" class="text" name="key_2_values[]" value="" />	
-
+							<hr class="clear" />
 							<label for="key_2_values">Relationship Value 3</label>
 							<input type="text" class="text" name="key_2_values[]" value="" />	
-
+							<hr class="clear" />
 						</fieldset>
 
 						<fieldset>
