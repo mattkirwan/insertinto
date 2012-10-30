c6ecee36525adbb3bd227d26ad57376d7e28a56c
Matt Kirwan <matthewkirwan@gmail.com>
2012-10-30 17:08:28 +0000

    Adding content

--- index.php
+++ index.php
@@ -5,6 +5,12 @@
 		<title>INSERT INTO .me - The MySQL INSERT Generator</title>
 	</head>
 	<body>
-
+		<h1>INSERT INTO .`me`</h1>
+		<h2>Quick and dirty MySQL INSERT generator.</h2>
+		<div id="content">
+			<p>We've all wrote quick script's to generate SQL queries that allow us to insert bulk data in one fell swoop.</p>
+			<p>I've hacked together this site to do just that. For now it's only really good at generating many to many schema's, the aim is to add more options over time.</p>
+			<p>Ultimately this idea is nothing new, like many other quick snippets of code it was nearly lost forever - until I realised that the best domain name in the world was available.</p>
+		</div>
 	</body>
 </html>
