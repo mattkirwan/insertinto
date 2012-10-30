a6a3f2e3f7e99a98bd17c4307767012dc85bba89
Matt Kirwan <matthewkirwan@gmail.com>
2012-10-30 21:57:06 +0000

    Added GA

--- includes/header.php
+++ includes/header.php
@@ -4,6 +4,19 @@
 	<head>
 		<title>INSERT INTO .me - The MySQL INSERT Generator</title>
 		<link type="text/css" href="/assets/css/style.css" rel="stylesheet" />
+		<script type="text/javascript">
+
+		  var _gaq = _gaq || [];
+		  _gaq.push(['_setAccount', 'UA-7628607-7']);
+		  _gaq.push(['_trackPageview']);
+
+		  (function() {
+		    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+		    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+		    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+		  })();
+
+		</script>		
 	</head>
 	<body>
 		<div id="container">
