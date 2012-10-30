478ce933195e62437150c39ab50fbcbeb404dbac
Matt Kirwan <matthewkirwan@gmail.com>
2012-10-30 21:32:38 +0000

    Whatif a relationship value field is empty

--- includes/footer.php
+++ includes/footer.php
@@ -1,4 +1,7 @@
 	
 		</div>
+		<div id="footer">
+			Created by <a href="https://twitter.com/mattkirwan" target="_blank" />@mattkirwan</a> | Wanna see the source? It's on <a href="https://github.com/mattkirwan/insertinto" target="_blank">GitHub</a> | Released under the <a href="http://opensource.org/licenses/gpl-2.0.php" target="_blank">GNU General Public License</a>
+		</div>
 	</body>
 </html>
