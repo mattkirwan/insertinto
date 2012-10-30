6d89380de16ca15b9134b8d14bbe1af49a582db8
Matt Kirwan <matthewkirwan@gmail.com>
2012-10-30 18:43:34 +0000

    Reshuffle

--- includes/generate.php
+++ includes/generate.php
@@ -9,12 +9,12 @@
 			case 'table':
 			case 'key_1_name':
 			case 'key_2_name':
-				$arrPost_Clean[$strKey] = filter_var($strKey, FILTER_SANITIZE_STRING);
+				$arrPost_Clean[$strKey] = filter_var($mxdValue, FILTER_SANITIZE_STRING);
 			break;
 
 			case 'from':
 			case 'to':
-				$arrPost_Clean[$strKey] = filter_var($strKey, FILTER_SANITIZE_NUMBER_INT);
+				$arrPost_Clean[$strKey] = filter_var($mxdValue, FILTER_SANITIZE_NUMBER_INT);
 			break;
 
 		}
