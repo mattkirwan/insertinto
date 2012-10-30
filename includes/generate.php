476976eb10db1fb65e35b4b37511faf4c71bbba0
Matt Kirwan <matthewkirwan@gmail.com>
2012-10-30 18:41:36 +0000

    Reshuffle

--- includes/generate.php
+++ includes/generate.php
@@ -1,5 +1,27 @@
 <?php
+
+	// Sanitize
+	foreach($_POST as $strKey => $mxdValue)
+	{
+		switch($strKey)
+		{
+			case 'db',
+			case 'table',
+			case 'key_1_name',
+			case 'key_2_name':
+				$arrPost_Clean[$strKey] = filter_var($strKey, FILTER_SANITIZE_STRING);
+			break;
+
+			case 'from',
+			case 'to':
+				$arrPost_Clean[$strKey] = filter_var($strKey, FILTER_SANITIZE_NUMBER_INT);
+			break;
+
+		}
+	}
+
 	echo '<pre>';
-	print_r($_POST);
+	print_r($arrPost_Clean);
 	echo '</pre>';
+
 ?>
