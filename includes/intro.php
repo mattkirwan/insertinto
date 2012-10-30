e1ae83f718f64a5aa524b3f2e7ad38d5a1cff08d
Matt Kirwan <matthewkirwan@gmail.com>
2012-10-30 21:48:58 +0000

    Whatif a relationship value field is empty

--- includes/intro.php
+++ includes/intro.php
@@ -1,3 +1,4 @@
-<p>We've all wrote quick script's to generate SQL queries that allow us to insert bulk data in one fell swoop.</p>
-			<p>I've hacked together this site to do just that. For now it's only really good at generating many to many schema's, the aim is to add more options over time.</p>
-			<p>Ultimately this idea is nothing new, like many other quick snippets of code it was nearly lost forever - until I realised that the best domain name in the world was available.</p>
+<p>We've all written quick script's to generate SQL queries that allow us to insert bulk data in one fell swoop.</p>
+			<p>I've hacked together this site to do just that. For now it can only generate many to many schema's, the aim is to add more options over time.</p>
+			<p>Ultimately this idea is nothing new, like many other quick snippets of code it was nearly lost forever - until I realised that the best domain name in the world was available...</p>
+			<p>...well it's either this or an 'naughty' website. I was buying it either way.</p>
